0f191726462d28ec4ff5d19c0aad893190c2ba17
MrCalvooo <terminatoresplandor@gmail.com>
2025-04-02 16:21:42 +0200

    lecturaFicheros

---
+++ Archivos/LecturaFicheros/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        LeerFichero leerFichero = new LeerFichero();
+
+        leerFichero.lee();
+    }
+}
---
+++ Archivos/LecturaFicheros/src/LeerFichero.java
@@ -0,0 +1,27 @@
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class LeerFichero {
+
+    public void lee() {
+        try {
+            FileReader entrada = new FileReader("mifichero.txt");
+
+            int c = entrada.read();
+            
+
+            while (c != -1) {
+                c = entrada.read();
+                char letra = (char) c;
+                System.out.print(letra);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("No se encontro el fichero: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Error de entrada: " + e.getMessage());
+        }
+    }
+}
