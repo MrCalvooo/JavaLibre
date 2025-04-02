63941a3cdab397dee624cc349a313b97fed5da23
MrCalvooo <terminatoresplandor@gmail.com>
2025-04-02 16:23:23 +0200

    cerramos lectura

--- Archivos/LecturaFicheros/src/LeerFichero.java
+++ Archivos/LecturaFicheros/src/LeerFichero.java
@@ -7,17 +7,15 @@ public class LeerFichero {
 
     public void lee() {
         try {
-            FileReader entrada = new FileReader("mifichero.txt");
-
-            int c = entrada.read();
-            
-
-            while (c != -1) {
-                c = entrada.read();
-                char letra = (char) c;
-                System.out.print(letra);
+            try (FileReader entrada = new FileReader("mifichero.txt")) {
+                int c = entrada.read();
+                
+                while (c != -1) {
+                    c = entrada.read();
+                    char letra = (char) c;
+                    System.out.print(letra);
+                }
             }
-
         } catch (FileNotFoundException e) {
             System.out.println("No se encontro el fichero: " + e.getMessage());
         } catch (IOException e) {
