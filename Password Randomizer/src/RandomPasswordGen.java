cc891cfbc6b7dcba64367896bbd7ca7bfc6b1cc7
MrCalvooo <terminatoresplandor@gmail.com>
2025-04-21 20:48:18 +0200

    optimizacion

--- Password Randomizer/src/RandomPasswordGen.java
+++ Password Randomizer/src/RandomPasswordGen.java
@@ -4,55 +4,60 @@ import java.util.Scanner;
 public class RandomPasswordGen {
 
     public static void main(String[] args) throws Exception {
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            String password = "";
+            int leng;
 
-        String password = "";
-        int leng;
+            System.out.println("Longitud deseada de la contraseña: ");
+            leng = scanner.nextInt();
+
+            for (int i = 0; i < leng; i++) {
+                int random = (int) (Math.random() * 4 + 1);
+                password += caracteresRandom(random);
+            }
+
+            System.out.println("Tu contraseña es: " + password);
+        }
+    }
+
+    public static String caracteresRandom(int random) {
 
         String minus = "abcdefghijklmnñopqrstuvwxyz";
         String mayus = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
         String nums = "0123456789";
         String especiales = ".-*/!ªº?¿'¡´ç¨{}_;:@#~";
+        String password = "";
+        char caracter;
 
-        System.out.println("Longitud deseada de la contraseña: ");
-        leng = scanner.nextInt();
-
-        for (int i = 0; i < leng; i++) {
-            int random = (int) (Math.random() * 4 + 1);
-            char caracter;
-
-            switch (random) {
-                case 1 -> {
-                    random = (int) (Math.random() * minus.length());
-                    caracter = minus.charAt(random);
-                    password += caracter;
-                }
-
-                case 2 -> {
-                    random = (int) (Math.random() * mayus.length());
-                    caracter = mayus.charAt(random);
-                    password += caracter;
-                }
-
-                case 3 -> {
-                    random = (int) (Math.random() * nums.length());
-                    caracter = nums.charAt(random);
-                    password += caracter;
-                }
-
-                case 4 -> {
-                    random = (int) (Math.random() * especiales.length());
-                    caracter = especiales.charAt(random);
-                    password += caracter;
-                }
-
-                default ->
-                    throw new AssertionError();
+        switch (random) {
+            case 1 -> {
+                random = (int) (Math.random() * minus.length());
+                caracter = minus.charAt(random);
+                password += caracter;
             }
+
+            case 2 -> {
+                random = (int) (Math.random() * mayus.length());
+                caracter = mayus.charAt(random);
+                password += caracter;
+            }
+
+            case 3 -> {
+                random = (int) (Math.random() * nums.length());
+                caracter = nums.charAt(random);
+                password += caracter;
+            }
+
+            case 4 -> {
+                random = (int) (Math.random() * especiales.length());
+                caracter = especiales.charAt(random);
+                password += caracter;
+            }
+
+            default ->
+                throw new AssertionError();
         }
 
-        System.out.println("Tu contraseña es: " + password);
-
-        scanner.close();
+        return password;
     }
 }
