8ed9e595ab713152d35710cea2472d215bcdb760
MrCalvooo <terminatoresplandor@gmail.com>
2025-11-05 00:28:45 +0100

    Creacion del XML mediante los datos del raf

--- ConjuntoEjercicios/EjerciciosXML/RafXML/src/Main.java
+++ ConjuntoEjercicios/EjerciciosXML/RafXML/src/Main.java
@@ -1,3 +1,15 @@
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
@@ -6,6 +18,13 @@ import java.io.RandomAccessFile;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        File file = new File("personas.dat");
+
+        if (file.exists()) {
+            file.delete();
+        }
+
         // Cada persona ocupará 68 bytes
         // 12 * 2 bytes el nombre, 10 * 2 bytes apellido1, 10 * 2 bytes apellido2, 4 bytes edad
         Persona[] personas = {
@@ -33,5 +52,68 @@ public class Main {
 
         System.out.println("-----------------------------------------------------------");
         System.out.println("Activando modo lectura para meter dicha información en un XML");
+        try {
+            RandomAccessFile raf = new RandomAccessFile("personas.dat", "r");
+
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document document = builder.newDocument();
+
+            Element root = document.createElement("personas");
+            document.appendChild(root);
+
+            raf.seek(0);
+
+            System.out.println("---------------------------------------------------");
+            System.out.println("Preparando datos para insertar en el XML");
+            while (raf.getFilePointer() < raf.length()) {
+                Element persona = document.createElement("persona");
+                char[] charNombre = new char[12];
+                for (int i = 0; i < charNombre.length; i++) {
+                    charNombre[i] = raf.readChar();
+                }
+                String nombre = new String(charNombre).trim();
+                Element nombreE = document.createElement("nombre");
+                nombreE.setTextContent(nombre);
+                persona.appendChild(nombreE);
+
+                char[] charApellidos = new char[10];
+                for (int i = 0; i < charApellidos.length; i++) {
+                    charApellidos[i] = raf.readChar();
+                }
+                String apellido1 = new String(charApellidos).trim();
+                Element apellido1E = document.createElement("apellido1");
+                apellido1E.setTextContent(apellido1);
+                persona.appendChild(apellido1E);
+
+                charApellidos = new char[10];
+                for (int i = 0; i < charApellidos.length; i++) {
+                    charApellidos[i] = raf.readChar();
+                }
+                String apellido2 = new String(charApellidos).trim();
+                Element apellido2E = document.createElement("apellido2");
+                apellido2E.setTextContent(apellido2);
+                persona.appendChild(apellido2E);
+
+                int edad = raf.readInt();
+                Element edadE = document.createElement("edad");
+                edadE.setTextContent(String.valueOf(edad));
+                persona.appendChild(edadE);
+
+                root.appendChild(persona);
+            }
+
+            DOMSource domSource = new DOMSource(document);
+            StreamResult result = new StreamResult("personas.xml");
+
+            System.out.println("--------------------------------------------------------------------");
+            System.out.println("Creando XML");
+            Transformer transformer = TransformerFactory.newDefaultInstance().newTransformer();
+
+            transformer.transform(domSource, result);
+            System.out.println("XML CREADO");
+        } catch (ParserConfigurationException | IOException | TransformerException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
